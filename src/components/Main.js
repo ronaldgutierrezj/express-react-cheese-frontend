64d5568dcf3ea30be5e90324cab3ac9d4ce67f9b
ronald <ronaldgutierrezj@gmail.com>
2021-11-11 10:13:58 -0600

    heroku updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import Show from "../pages/Show";
 
 function Main(props){
     const[cheese, setCheese] = useState (null);
-    const URL = "https://express-react-cheese-backend.herokuapp.com/cheese/";
+    const URL = "https://ringo-cheese-app-backend.herokuapp.com/cheese/";
 
     const getCheese = async() => {
         const response = await fetch(URL);
